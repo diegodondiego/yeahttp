7e773812d0f6e3ecf90deaa43dd9a1d8608fb8cc
Diego Martins <dondiego7@gmail.com>
2024-02-28 17:50:18 -0800

    generating the implementation - skeleton

--- src/main/java/org/isscorp/yeahttp/generators/ImplGenerator.java
+++ src/main/java/org/isscorp/yeahttp/generators/ImplGenerator.java
@@ -5,21 +5,29 @@ import java.lang.reflect.Proxy;
 /**
  * This class is the main class to generate the implementations of the annotated objects
  * TODO rename it for a better name
+ *
+ * @param <T> the type of the class to be enriched
  */
 public class ImplGenerator<T> {
 
   private final Class<T> annotatedInterface;
 
-  public ImplGenerator(final Class<T> annotatedInterface) {
+  /**
+   * @param annotatedInterface the type to be enriched with the capabilities of the framework
+   */
+  public ImplGenerator(Class<T> annotatedInterface) {
     this.annotatedInterface = annotatedInterface;
-    if (annotatedInterface == null || !annotatedInterface.isInterface()) {
+    if (null == annotatedInterface || !annotatedInterface.isInterface()) {
       throw new IllegalArgumentException("object " + annotatedInterface + "is not an interface");
     }
   }
 
+  /**
+   * @return an implemented instance of T
+   */
   public T generateImplementation() {
 
-    return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{this.annotatedInterface},
+    return (T) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[]{annotatedInterface},
         new DefaultInvocationHandler());
   }
 
